4b1ef2c91f3df9096fbaae88b3379b920add0649
rev022 <92462704+rev022@users.noreply.github.com>
2021-10-29 23:28:50 +0530

    Add files via upload

---
+++ DAY1_ASSIGNMENT/ArraySpecific.java
@@ -0,0 +1,22 @@
+package com.sonata;
+
+public class ArraySpecific {
+
+	  public static void main(String[] args) {  
+
+		  
+
+		  int[] myArray = {3, 4, 6, 10};
+	      int num = 4;
+
+	      for(int i = 0; i<myArray.length; i++){
+	         if(num == myArray[i]){
+	            System.out.println("Array contains the given element");
+	         }
+	      
+	         
+	      }
+	         
+	      }
+	   
+}
