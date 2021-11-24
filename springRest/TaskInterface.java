7ae0dae085278edc04b0e326569f83f874de99df
rev022 <92462704+rev022@users.noreply.github.com>
2021-11-24 10:47:49 +0530

    Add files via upload
    pushing assignments

---
+++ springRest/TaskInterface.java
@@ -0,0 +1,12 @@
+package com.sonata.Rest;
+
+import java.util.List;
+
+
+public interface TaskInterface {
+	//public Task getTask(int id);
+	public List<Task> getData();
+	public int setPriority(int taskid, String pri);
+	public List<Task> printTask(int t);
+	public int addtask(Task u);
+}
